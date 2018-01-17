117986f25beaa92f3942325a995b18ac76580fe9
Muhammad Muizuddin <Muhammad Muizuddin@MuhammadMuizuddin-LAPTOP-JO7D1JT3>
2018-01-17 23:02:52 +0800

    Commit version 3.0

--- src/Discount.java
+++ src/Discount.java
@@ -23,7 +23,7 @@ public class Discount {
         System.out.println("Sila masukkan jumlah harga pembelian anda: ");
         amount= input.nextDouble();
         
-        if (amount<100000000){
+        if (amount<1000000000){
             discount='0';
             
         }
